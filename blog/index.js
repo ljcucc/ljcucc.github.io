b3ed152eaa1d0be8eb9b11900113471fd35af901
ljcucc <linjason920523@gmail.com>
2020-08-07 00:09:41 +0800

    create blog

---
+++ blog/index.js
@@ -0,0 +1,6 @@
+window.addEventListener("load",()=>{
+  let content = document.querySelector(".content");
+  document.querySelector("#refresh").addEventListener("click", ()=>{
+    content.innerHTML = "Opss.... this function mid not be finished yet. try again some days while we keep developing.";
+  })
+});
