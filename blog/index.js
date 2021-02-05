15fb82f08c88def2121d83f59c9599f129c28921
ljcucc <linjason920523@gmail.com>
2021-02-05 14:47:21 +0800

    create new angular project

--- blog/index.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load",()=>{
-  let content = document.querySelector(".content");
-  document.querySelector("#refresh").addEventListener("click", ()=>{
-    content.innerHTML = "Opss.... this function mid not be finished yet. try again some days while we keep developing.";
-  })
-});
